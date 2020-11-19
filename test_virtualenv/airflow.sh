d1e0c7e97d8cee540abb6470d43d8acb27951d08
Andrea Bonomi <andrea.bonomi@italiaonline.it>
2020-11-19 07:45:02 +0100

    testenv for Airflow 2 versions

--- test_virtualenv/airflow.sh
+++ test_virtualenv/airflow.sh
@@ -3,5 +3,13 @@ cd "$(dirname "$0")"
 if [[ ! -a bin/activate ]]; then
     ./create_test_virtualenv.sh
 fi
+
 source bin/activate
+
+if compgen -G "../dist/*.whl" > /dev/null; then
+    echo "Install plugin"
+    pip install --no-deps ../dist/*.whl --force
+    rm ../dist/*.whl
+fi
+
 ./bin/airflow "$@"
--- test_virtualenv/create_test_virtualenv.sh
+++ test_virtualenv/create_test_virtualenv.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit 1
 source ./mo.sh
 export AIRFLOW_HOME=${PWD}
 
 export ENABLE_AIRFLOW_AUTH=1
-export AIRFLOW_VERSION=1.10.12
+# export AIRFLOW_VERSION=1.10.12
+export AIRFLOW_VERSION=2.0.0b2
 # export FORCE_PYTHON_VERSION=3.7
+
+export AIRFLOW_MAJOR_VERSION=${AIRFLOW_VERSION:0:1}
+if [[ "${AIRFLOW_MAJOR_VERSION}" == "2" ]]; then
+    export CONSTRAINTS_VERSION="2-0"
+    export EXTRAS="password"
+else
+    export CONSTRAINTS_VERSION="${AIRFLOW_VERSION}"
+    export EXTRAS="crypto,password"
+fi
 
 function WEBSERVER_AUTH() {
     if [[ $ENABLE_AIRFLOW_AUTH == 1 ]]; then
@@ -25,18 +35,27 @@ pip install --upgrade pip
 
 export PYTHON_VERSION=$(python3 -c "import sys; print('%s.%s' % (sys.version_info.major, sys.version_info.minor))")
 pip install \
-     apache-airflow[crypto,password]==${AIRFLOW_VERSION} \
-      --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+     apache-airflow[${EXTRAS}]==${AIRFLOW_VERSION} \
+      --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-${CONSTRAINTS_VERSION}/constraints-${PYTHON_VERSION}.txt"
 
 # pip install --no-deps airflow-code-editor
 rm -rf plugins
 mkdir -p plugins
-ln -sf "${PWD}/../airflow_code_editor" plugins/airflow_code_editor
+# ln -sf "${PWD}/../airflow_code_editor" plugins/airflow_code_editor
 
-mo -u < airflow.cfg.tmpl > airflow.cfg
-airflow initdb
+if [[ "${AIRFLOW_MAJOR_VERSION}" == "1" ]]; then
+    mo -u < airflow1.cfg.tmpl > airflow.cfg
+    airflow initdb
+else
+    mo -u < airflow2.cfg.tmpl > airflow.cfg
+    airflow db init
+fi
 
 if [[ $ENABLE_AIRFLOW_AUTH == 1 ]]; then
     # Create user 'admin' with password 'admin'
-    airflow create_user -r Admin -u admin -e admin@example.com -f admin -l admin -p admin
+    if [[ "${AIRFLOW_MAJOR_VERSION}" == "1" ]]; then
+        airflow create_user -r Admin -u admin -e admin@example.com -f admin -l admin -p admin
+    else
+        airflow users create -r Admin -u admin -e admin@example.com -f admin -l admin -p admin
+    fi
 fi

457bef091bb6b326eba4d0982ba23105f93c4747
Andrea Bonomi <andrea.bonomi@italiaonline.it>
2021-03-13 10:17:21 +0100

    testenv for Airflow 2.0.1

--- test_virtualenv/airflow.sh
+++ test_virtualenv/airflow.sh
@@ -9,7 +9,9 @@ source bin/activate
 if compgen -G "../dist/*.whl" > /dev/null; then
     echo "Install plugin"
     pip install --no-deps ../dist/*.whl --force
-    rm ../dist/*.whl
+else
+    pip uninstall airflow-code-editor &>/dev/null
+    export PYTHONPATH=..:$PYTHONPATH
 fi
 
 ./bin/airflow "$@"

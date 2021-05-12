771745d89487efad03399e8024ad5f527ac36678
Andrea Bonomi <andrea.bonomi@italiaonline.it>
2021-05-12 11:18:26 +0200

    Fix activate.sh script

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-set -e
-
 export CURRENT_DIR=`realpath $(dirname "$0")`
 export PARENT_DIR=`dirname "$CURRENT_DIR"`
 
 if [[ ! -a "${CURRENT_DIR}/venv/bin/activate" ]]; then
-    source "${CURRENT_DIR}/create_test_virtualenv.sh"
+    bash "${CURRENT_DIR}/create_test_virtualenv.sh"
 fi
 
 source "${CURRENT_DIR}/venv/bin/activate"
-source "${CURRENT_DIR}/install.sh"
+bash "${CURRENT_DIR}/install.sh"

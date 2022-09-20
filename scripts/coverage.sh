02ea66d63088cc3f2578609952ddadebcf72700a
Andrea Bonomi <andrea.bonomi@gmail.com>
2022-09-20 08:55:01 +0000

    test coverage

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -5,4 +5,4 @@ export CURRENT_DIR=`realpath $(dirname "$0")`
 source "${CURRENT_DIR}/activate.sh"
 
 cd `dirname "$CURRENT_DIR"`
-python3 -m coverage run --source=airflow_code_editor setup.py test && python3 -m coverage report -m
+pytest --cov --cov-report=term-missing

b69ac5c38f6ae1a5376d5d97d2cdd3095815c388
Andrea Bonomi <andrea.bonomi@jakala.com>
2024-08-26 12:01:16 +0200

    Change Airflow test version to 2.10.0

--- tests/test_app_builder_view.py
+++ tests/test_app_builder_view.py
@@ -1,65 +1,75 @@
 #!/usr/bin/env python
 
-# from airflow import configuration
-from flask import Flask
-from flask_appbuilder import SQLA, AppBuilder
+import pytest
+from airflow.www import app as application
 
 from airflow_code_editor.app_builder_view import appbuilder_code_editor_view
 from airflow_code_editor.commons import HTTP_200_OK, HTTP_400_BAD_REQUEST, ROUTE
 
-app = Flask(__name__)
-app.testing = True
-app.config['SECRET_KEY'] = "000000000000000000000000"
-db = SQLA(app)
-appbuilder = AppBuilder(app, db.session)
-appbuilder.add_view_no_menu(appbuilder_code_editor_view)
-appbuilder.sm.check_authorization = lambda *args, **kargs: True
-client = app.test_client()
+
+@pytest.fixture
+def app():
+    return application.create_app(testing=True)
 
 
-def get(path, *args, **kargs):
+@pytest.fixture
+def app_builder(app):
+    appbuilder = app.appbuilder
+    appbuilder.add_view_no_menu(appbuilder_code_editor_view)
+    appbuilder.sm.check_authorization = lambda *args, **kargs: True
+    return appbuilder
+
+
+def get(path, app, *args, **kargs):
     with app.test_client() as client:
         return client.get(f"{ROUTE}{path}", *args, **kargs)
 
 
-def post(path, *args, **kargs):
+def post(path, app, *args, **kargs):
     with app.test_client() as client:
         return client.post(f"{ROUTE}{path}", *args, **kargs)
 
 
-def test_ping():
-    res = get("/ping")
+def test_ping(app, app_builder):
+    res = get("/ping", app)
     assert res.status_code == HTTP_200_OK
-    assert len(res.json['value']) > 10
+    assert len(res.json["value"]) > 10
 
 
-def test_tree():
-    res = get("/tree")
+def test_tree(app, app_builder):
+    res = get("/tree", app)
     assert res.status_code == HTTP_200_OK
-    assert res.json['value']
-    assert 'files' in [x['id'] for x in res.json['value']]
-    assert 'files/~airflow_home' in [x['id'] for x in res.json['value']]
-    assert 'git' in [x['id'] for x in res.json['value']]
+    assert res.json["value"]
+    assert "files" in [x["id"] for x in res.json["value"]]
+    assert "files/~airflow_home" in [x["id"] for x in res.json["value"]]
+    assert "git" in [x["id"] for x in res.json["value"]]
 
 
-def test_tree_git():
-    res = get("/tree/git")
+def test_tree_git(app, app_builder):
+    res = get("/tree/git", app)
     assert res.status_code == HTTP_200_OK
-    assert res.json['value']
+    assert res.json["value"]
 
 
-def test_tree_local_branches():
-    res = get("/tree/local-branches")
+def test_tree_local_branches(app, app_builder):
+    res = get("/tree/local-branches", app)
     assert res.status_code == HTTP_200_OK
-    assert res.json['value']
+    assert res.json["value"]
 
 
-def test_tree_files():
-    res = get("/tree/files")
+def test_tree_files(app, app_builder):
+    res = get("/tree/files", app)
     assert res.status_code == HTTP_200_OK
-    assert res.json['value']
+    assert res.json["value"]
 
 
-def test_repo():
-    res = post("/repo")
+def test_repo(app, app_builder):
+    res = post("/repo", app)
     assert res.status_code == HTTP_400_BAD_REQUEST
+
+
+def test_version(app, app_builder):
+    res = get("/version", app)
+    assert res.status_code == HTTP_200_OK
+    assert "version" in res.json
+    assert "airflow_version" in res.json
